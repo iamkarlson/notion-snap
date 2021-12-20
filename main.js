ffda5fb0597fe4374d3027b0cab0eef4198f1d34
Georgy Grigoryev <iamkarlson@gmail.com>
2021-12-20 09:24:39 +0100

    navigate out

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const { app, BrowserWindow } = require("electron");
 
+function postConfigure(window) {
+    window.webContents.on("will-navigate", function (event, reqUrl) {
+        alert('will-navigate');
+        let requestedHost = new URL(reqUrl).host;
+        let currentHost = new URL(window.webContents.getURL()).host;
+        if (requestedHost && requestedHost != currentHost) {
+            event.preventDefault();
+            shell.openExternal(reqUrl);
+        }
+    });
+}
+
 app.on("ready", () => {
-  const win = new BrowserWindow({
-    autoHideMenuBar: true,
-  });
-  win.maximize();
-  win.loadURL("https://notion.so");
+    const win = new BrowserWindow({
+        autoHideMenuBar: true,
+        icon: __dirname + '/notion.ico',
+    });
+    win.maximize();
+    win.loadURL("https://notion.so");
+    alert('ready')
+    postConfigure(win);
+
 });
